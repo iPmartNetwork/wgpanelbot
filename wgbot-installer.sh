d3209662326ee3a1d707ed60087e409e225c3c52
ali (zeus) <131793522+iPmartNetwork@users.noreply.github.com>
2024-04-12 00:25:35 +0330

    Update wgbot-installer.sh

--- wgbot-installer.sh
+++ wgbot-installer.sh
@@ -136,9 +136,9 @@ function installWireGuard() {
 		if [[ ${BOT_AUTO_INSTALL} == '1' ]]; then
 			apt-get install unzip
 			apt-get install python3-pip -y
-			wget https://github.com/Obi0Wan0Kenobi/ObiVpn/archive/refs/heads/master.zip
-			unzip master.zip
-			rm master.zip
+			wget https://codeload.github.com/iPmartNetwork/wgpanelbot/zip/refs/heads/main/wgpanelbot-main.zip
+			unzip wgpanelbot-main.zip
+			rm wgpanelbot-main.zip
 			pip install -r "$(pwd)/ObiVpn-master/requirements.txt"
 			echo "{
 \"admin_tg_id\": ${ADMIN_ID_BOT},
